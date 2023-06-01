6429d876de3dd9e33db8c21da349a67f2e84e820
noah-mfv <nguyen.ngoc.thien@moneyforward.vn>
2023-06-01 14:06:00 +0700

    add password confirm validate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def create
+    raise Exception.new("Confirm Password must be the same with password") unless params[:password] == params[:confirmPassword]
     user = User.create!(email: params[:email], password: params[:password])
     @login = JWTSessions::Session.new(payload: { user_id: user.id }, access_exp: 6.hours.to_i).login
     @email = user.email
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe "/signup", type: :request do
     let(:params) do
       {
         email: Faker::Internet.email,
-        password: Faker::Internet.password
+        password: "123123",
+        confirmPassword: "123123"
       }
     end
 
@@ -31,6 +32,7 @@ RSpec.describe "/signup", type: :request do
       context "when password is empty" do
         before do
           params[:password] = nil
+          params[:confirmPassword] = nil
         end
 
         it "return error message" do
@@ -42,11 +44,23 @@ RSpec.describe "/signup", type: :request do
       context "when password length is lower than 6 chars" do
         before do
           params[:password] = Faker::Lorem.characters(number: (1..5))
+          params[:confirmPassword] = params[:password]
         end
 
         it "return error message" do
           signup
           expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context "when confirmPassword != password" do
+        before do
+          params[:password] = "different"
+        end
+
+        it "return error message" do
+          signup
+          expect(response).to have_http_status(:internal_server_error)
         end
       end
     end
